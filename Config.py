150f77e182560ec2e3309aa43cd9fba397f0970a
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-05-08 03:00:12 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -5,7 +5,9 @@ API_HASH = os.getenv("API_HASH", "94e17044c2393f43fda31d3afe77b26b")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7849290437:AAGsPd0m4de-bcNXYNvqb8fgBQ5eNHXyc2Q")
 MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://Sinchu:Sinchu@sinchu.qwijj.mongodb.net/?retryWrites=true&w=majority&appName=Sinchu")
 
-OWNER_ID = int(os.getenv("OWNER_ID", 8091116698, 7862043458))
+OWNER_IDS = [int(x.strip()) for x in os.getenv("OWNER_ID", "8091116698,7849290437").split(",")]
+OWNER_ID = OWNER_IDS[0]
+
 LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID", -1002287107269))
 
-SUDO_USERS = [OWNER_ID]  # Extendable via /addsudo
+SUDO_USERS = list(set(OWNER_IDS + [int(x.strip()) for x in os.getenv("SUDO_USERS", "").split(",") if x.strip().isdigit()]))
