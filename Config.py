88fea6254788f3fe75b391478364616e09ee8b00
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-05-08 03:27:39 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -6,18 +6,15 @@ BOT_TOKEN = os.getenv("BOT_TOKEN", "7849290437:AAGsPd0m4de-bcNXYNvqb8fgBQ5eNHXyc
 MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://Sinchu:Sinchu@sinchu.qwijj.mongodb.net/?retryWrites=true&w=majority&appName=Sinchu")
 
 # Handle multiple OWNER_IDs as a list
-OWNER_IDS = list(map(int, os.getenv("OWNER_ID", "7862043458").split(",")))
+OWNER_IDS = os.getenv("OWNER_ID", "7862043458").split(",")
+OWNER_IDS = [int(x.strip()) for x in OWNER_IDS if x.strip().isdigit()]
 
 # Primary owner for specific logic
-OWNER_ID = OWNER_IDS[0]
+OWNER_ID = OWNER_IDS[0] if OWNER_IDS else None
 
 LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID", -1002287107269))
 
 # SUDO_USERS includes all OWNER_IDS by default, plus others if set
-SUDO_USERS = list(
-    set(
-        OWNER_IDS + list(
-            map(int, os.getenv("SUDO_USERS", "").split(","))
-        )
-    )
-)
+SUDO_USERS = os.getenv("SUDO_USERS", "").split(",")
+SUDO_USERS = list(set(OWNER_IDS + [int(x.strip()) for x in SUDO_USERS if x.strip().isdigit()]))
+
