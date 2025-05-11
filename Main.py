c84714fa486a99f117f48c3838a7c1e4a297356d
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-05-11 11:02:10 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,13 +7,17 @@ import asyncio
 import importlib
 import glob
 
+plugin_folder = "Plugins"
+for filename in os.listdir(plugin_folder):
+    if filename.endswith(".py"):
+        importlib.import_module(f"{plugin_folder}.{filename[:-3]}")
+
 
 bot = Client(
     "RichFeatureBot",
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
     bot_token=Config.BOT_TOKEN,
-    plugins={"root": "Plugins"}
 )
 
 if __name__ == "__main__":
