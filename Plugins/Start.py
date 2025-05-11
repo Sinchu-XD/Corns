7b467c1a7ab23d200749b74007d7ff539f2f5bd1
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-05-11 20:55:53 +0530

    Update Start.py

--- Plugins/Start.py
+++ Plugins/Start.py
@@ -24,7 +24,7 @@ async def start(client, message):
 
     if not_joined:
         buttons = [
-            [InlineKeyboardButton(f"📢 Join @{ch}", url=f"https://t.me/{ch}")]
+            [InlineKeyboardButton(f"Join {ch}", url=f"https://t.me/{ch.replace('@', '')}")])
             for ch in not_joined
         ]
         buttons.append([InlineKeyboardButton("✅ I've Joined", callback_data="check_join")])
