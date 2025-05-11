7b0dce40da3150d86eeb755eb5521ef5258ad81c
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-05-11 12:57:14 +0530

    Update Store.py

--- Plugins/Store.py
+++ Plugins/Store.py
@@ -5,8 +5,8 @@ from pyrogram.types import Message
 from Database import save_file
 from Bot import bot
 from Config import Config
-
-@bot.on_message(filters.private & filters.media)
+from Decorators import owner_or_sudo
+@bot.on_message(filters.private & filters.media & owner_or_sudo)
 async def handle_file(c: Client, m: Message):
     media = m.photo or m.video or m.document
     if not media:
