0277eda52c6a006844d4b1cbe557b52cb8a4485b
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-05-12 12:43:03 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -5,10 +5,10 @@ import os
 class Config:
     API_ID = int(os.getenv("API_ID", 6067591))
     API_HASH = os.getenv("API_HASH", "94e17044c2393f43fda31d3afe77b26b")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "7811511513:AAFE_MgOT1iqcmn9wnguyWk5sah8AwsdexA")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "7552388702:AAGPgiFWRuAeKyI70Zqrn49IpYalb5xBb6I")
     MONGO_URI = os.getenv("MONGO_URL", "mongodb+srv://Sinchu:Sinchu@sinchu.qwijj.mongodb.net/?retryWrites=true&w=majority&appName=Sinchu")
 
     OWNER_ID = int(os.environ.get("OWNER_ID", "7862043458"))  # Your Telegram user ID
     LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID", -1002251790583))
     
-    BOT_USERNAME = "CornHub69Bot"
+    BOT_USERNAME = "HanimeStuffBot"
