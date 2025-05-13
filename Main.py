f52e83b978eeded36b53d866deaec5eae64a3dcc
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-05-13 08:37:12 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,18 +1,14 @@
-# main.py
-from Bot import bot
+from Bot import client
 import os
-import asyncio
 import importlib
-import glob
 
 plugin_folder = "Plugins"
+
+# Dynamically import all plugins from Plugins folder
 for filename in os.listdir(plugin_folder):
     if filename.endswith(".py"):
         importlib.import_module(f"{plugin_folder}.{filename[:-3]}")
 
-
-
 if __name__ == "__main__":
     print(">> Bot Starting...")
-    bot.run()
-    
+    client.run_until_disconnected()
